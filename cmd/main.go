d3dbf9fa922e933a16709a499f4f81646fef57a3
Калинин Сергей <sergey.kalinin@sebbia.org>
2024-06-20 12:09:06 +0300

    v1.0.2: fix playground

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/sergey23144V/BotanyBackend/pkg/repository"
 	"github.com/sergey23144V/BotanyBackend/pkg/service"
 	g_rpc "github.com/sergey23144V/BotanyBackend/servers/g-rpc"
@@ -8,17 +9,23 @@ import (
 	"github.com/sergey23144V/BotanyBackend/servers/graphql"
 	"github.com/sergey23144V/BotanyBackend/servers/rest"
 	"log"
+	"os"
 )
 
 func main() {
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Ошибка загрузки файла .env: %v", err)
+	}
+
 	cfgDB := repository.Config{
-		Host:     "db",
-		Port:     "5432",
-		Username: "postgres",
-		DBName:   "botany",
-		SSLMode:  "disable",
-		Password: "123321",
+		Host:     os.Getenv("HOST"),
+		Port:     os.Getenv("PORT"),
+		Username: os.Getenv("USER"),
+		DBName:   os.Getenv("DB"),
+		SSLMode:  os.Getenv("SSLMode"),
+		Password: os.Getenv("PASSWORD"),
 	}
 
 	db, err := repository.ConnectDB(cfgDB)
--- go.mod
+++ go.mod
@@ -41,6 +41,7 @@ require (
 	github.com/jackc/pgx/v5 v5.4.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/matryer/is v1.4.1 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
